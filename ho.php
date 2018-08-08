f21c2799ccbed0136b97fb499a650ee9e8f768be
harshme <hs299348@gmail.com>
2018-08-08 20:24:37 +0530

    first commit

---
+++ ho.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title></title>
+		<style>
+			.main{width:100%; height:700px;background:url(1.jpg);overflow:auto;}
+		::-webkit-scrollbar{width:0px;background:transparent;}
+			.a{width:100%; height:1000px;background:red;opacity:.5;}
+			.b{width:100%; height:1000px;background:blue;opacity:.1}
+			.c{width:100%; height:1000px;background:black;opacity:.3;}
+		</style>
+	</head>
+	<body>
+		<div class="main">
+			<div class="a"></div>
+			<div class="b"></div>
+			<div class="c"></div>
+			
+		</div>
+	</body>
+</html>
---
+++ superhero.php
@@ -0,0 +1,315 @@
+<html>
+	<head>
+		<title></title>
+		<style>
+				.main{width:100%; height:700px;background:url(1.jpg);background-size:100% 100%;overflow:auto;}	
+			::-webkit-scrollbar {
+    width: 0px;
+    background: transparent;
+}
+				.a{width:100%;height:2500px;float:left;}
+						.a1{width:100%;height:500px;float:left;}
+						.text1{
+				float:left;
+				font-size:80px;
+				text-align:center;
+				color:red;
+				text-shadow:4px 4px 8px white;
+				width:100%;
+				height:80px;
+				text-decoration:line-through;
+				margin-top:210px;
+				animation-name:my;
+				animation-duration:16s;
+				animation-iteration-count:infinite;
+			}
+			@keyframes my{
+				0%{
+					transform:rotateY(0deg);
+				}
+				12.5%{
+					transform:rotateY(45deg);
+				}
+				25%{
+					transform:rotateY(90deg);
+				}
+				37.5%{
+					transform:rotateY(135deg);
+				}
+				50%{
+					transform:rotateY(180deg);
+				}
+				62.5%{
+					transform:rotateY(225deg);
+				}
+				75%{
+					transform:rotateY(270deg);
+				}
+				87.5%{
+					transform:rotateY(315deg);
+				}
+				100%{
+					transform:rotateY(360deg);
+			}}
+						.a2{width:100%;height:500px;float:left;background:url(2.jpg);background-size:100% 100%;opacity:1;}
+						.text2{
+				float:left;
+				font-size:80px;
+				text-align:center;
+				color:teal;
+				text-shadow:4px 4px 8px white;
+				width:100%;
+				height:80px;
+				margin-top:210px;
+				animation-name:my;
+				animation-duration:16s;
+				animation-iteration-count:infinite;
+			}
+			@keyframes my{
+				0%{
+					transform:rotateX(0deg);
+				}
+				12.5%{
+					transform:rotateX(-45deg);
+				}
+				25%{
+					transform:rotateX(-90deg);
+				}
+				37.5%{
+					transform:rotateX(-135deg);
+				}
+				50%{
+					transform:rotateX(-180deg);
+				}
+				62.5%{
+					transform:rotateX(-225deg);
+				}
+				75%{
+					transform:rotateX(-270deg);
+				}
+				87.5%{
+					transform:rotateX(-315deg);
+				}
+				100%{
+					transform:rotateX(-360deg);
+			}}
+						.a3{width:100%;height:500px;float:left;}
+						.text3{
+				float:left;
+				font-size:80px;
+				text-align:center;
+				color:green;
+				text-shadow:4px 4px 8px white;
+				width:100%;
+				height:80px;
+				text-decoration:line-through;
+				margin-top:210px;
+				animation-name:my;
+				animation-duration:12s;
+				animation-iteration-count:infinite;
+			}
+			@keyframes my{
+				0%{
+					transform:rotateY(0deg);
+				}
+				100%{
+					transform:rotateY(360deg);
+			}}
+			
+						.a4{width:100%;height:500px;float:left;background:url(3.png);background-size:100% 100%;opacity:1;}
+						.text4{
+				float:left;
+				font-size:80px;
+				text-align:center;
+				color:black;
+				text-shadow:4px 4px 8px white;
+				width:100%;
+				height:80px;
+				margin-top:210px;
+				animation-name:my;
+				animation-duration:8s;
+				animation-iteration-count:infinite;
+			}
+			@keyframes my{
+				0%{
+					transform:rotateY(0deg);
+				}
+				12.5%{
+					transform:rotateY(45deg);
+				}
+				25%{
+					transform:rotateY(90deg);
+				}
+				37.5%{
+					transform:rotateY(135deg);
+				}
+				50%{
+					transform:rotateY(180deg);
+				}
+				62.5%{
+					transform:rotateY(225deg);
+				}
+				75%{
+					transform:rotateY(270deg);
+				}
+				87.5%{
+					transform:rotateY(315deg);
+				}
+				100%{
+					transform:rotateY(360deg);
+			}}
+						
+						.a5{width:100%;height:500px;float:left;}
+						.text5{
+				float:left;
+				font-size:80px;
+				text-align:center;
+				color:purple;
+				text-shadow:4px 4px 8px white;
+				width:100%;
+				height:80px;
+				text-decoration:line-through;
+				margin-top:210px;
+				animation-name:my;
+				animation-duration:20s;
+				animation-iteration-count:infinite;
+			}
+			@keyframes my{
+				0%{
+					transform:rotateY(0deg);
+				}
+				12.5%{
+					transform:rotateY(45deg);
+				}
+				25%{
+					transform:rotateY(90deg);
+				}
+				37.5%{
+					transform:rotateY(135deg);
+				}
+				50%{
+					transform:rotateY(180deg);
+				}
+				62.5%{
+					transform:rotateY(225deg);
+				}
+				75%{
+					transform:rotateY(270deg);
+				}
+				87.5%{
+					transform:rotateY(315deg);
+				}
+				100%{
+					transform:rotateY(360deg);
+			}}
+						
+						
+				.b{width:100%;height:700px;background:white;float:left;background:url(giphy_8.gif);background-size:100% 100%;}		
+						.b1{width:50%;height:700px;float:left;}
+						.b11{font-size:50px;color:red;text-indent:200px;line-height:15px;font-family:Jokerman;margin-top:50px;}
+						.b2{width:50%;height:700px;float:left;}
+						.b21{font-size:50px;color:red;text-indent:300px;line-height:38px;font-family:Jokerman;}
+
+												
+				.c{width:100%;height:1200px;float:left;background:url(giphy_11.gif);background-size:100%;}	
+						.c2{width:50%;height:400px;float:left;}
+						.c1{width:50%;height:400px;float:left;background:url(giphy_10.gif);background-size:100% 100%;opacity:.5;}
+						.c3{width:50%;height:400px;float:left;}					
+						.c6{width:50%;height:400px;float:left;}					
+						.c5{width:50%;height:400px;float:left;background:url(giphy_13.gif);background-size:100% 100%;}
+						.c4{width:50%;height:400px;float:left;background:url(giphy_12.gif);background-size:100% 100%;opacity:.5;}
+						.c21{color:white;font-size:50px;}
+						.c31{color:white;font-size:50px;}
+						.c61{color:white;font-size:50px;}
+	
+	
+	
+					.d{width:100%;height:1400px;float:left;}
+					.d1{width:100%;height:700px;float:left;background:url(giphy_7.gif);background-size:100% 100%;}
+					.d2{width:100%;height:700px;float:left;background:url(giphy_9.gif);background-size:100% 100%;}
+					.d11{font-size:60px;color:red;line-height:600px;text-decoration:bold;text-indent:90px;}
+					.d21{font-size:50px;color:teal;line-height:50px;text-decoration:bold;text-indent:60px;margin-top:400px;margin-left:200px;}
+					.d22{font-size:40px;color:red;line-height:50px;text-decoration:bold;text-indent:310px;}
+					.d23{font-size:30px;color:blue;line-height:100px;text-decoration:bold;text-indent:400px;}
+							
+		</style>
+	</head>
+	<body>
+	<div class="main">
+		<div class="a">
+				<div class="a1">
+					<p class="text1">WELCOME TO WHOLE NEW WORLD</p>
+				</div>
+				<div class="a2">
+						<p class="text2">WORLD FULL OF WONDERS</p>
+				</div>
+				<div class="a3">
+				       <p class="text3">WORLD FULL OF SUPRISES</p>
+				</div>
+				<div class="a4">
+				       <p class="text4">WORLD FULL OF MAGIC</p>
+				</div>
+				<div class="a5">
+				       <p class="text5">WORLD FULL OF ILLUSION</p>
+				</div>				
+		</div>
+		<div class="b">
+				
+				<div class="b1">
+						<p class="b11">WORLD</p>
+						<p class="b11">  IS  </p>
+						<p class="b11">WHAT</p>
+						<p class="b11">  WE </p>
+						<p class="b11">THINK</p>
+						<p class="b11">NOT</p>
+						<p class="b11">WHAT</p>
+						<p class="b11">  IS  </p>
+						<p class="b11">REAL!!!</p>
+				</div>
+				<div class="b2">
+				
+						<p class="b21">LET'S</p>
+						<p class="b21">TEST </p>
+						<p class="b21">HOW</p>
+						<p class="b21">YOU</p>
+						<p class="b21">SEE</p>
+						<p class="b21">THE</p>
+						<p class="b21">WORLD???</p>
+				</div>
+		</div>
+		<div class="c">
+				
+				<div class="c1">
+				</div>
+				<div class="c2">
+					<p class="c21">IS IT REALLY A CUBE?</p>
+				</div>
+				<div class="c3">
+					<p class="c31">HOW CAN A BALL CLIMB UP??  </p>
+				</div>
+				<div class="c4">
+				</div>
+				<div class="c5">
+				</div>
+				<div class="c6">
+					<p class="c61">DOES SHE HAS DUAL FACE??</p>
+				</div>
+		</div>
+		<div class="d">
+				<div class="d1">
+				     <p class="d11">CAN U WATCH IT CONTINOUSLY FOR 30 SEC ?</p>
+				</div>
+				<div class="d2">
+					<p class="d21">KEEP THINKING ABOUT WHAT U JUST SAW!!!</p>
+					<p class="d22">ILLUSION OR REALITY OR BOTH?</p>
+					<p class="d23">FIND OUT YOURSELF!!!!</p>
+
+				</div>
+		</div>
+	</div>
+	
+	<audio controls autoplay loop >
+	<source class="audio" src="1.mp3" type="audio/ogg" >
+	</audio>
+	</body>
+</html>
